41564e1f673fe43c9cc1b84c42a0591a619fb4da
Brage Skjonborg <brage.skjonborg@hotmail.no>
2021-12-11 23:46:19 +0100

    👌 Added multi-domain service

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dip-dup
 Dynamic IP - Dns Updater: Linux (systemd) service that pulls dns record and verifies that the IP on dns and IP on nic is the same
+
+Make sure that your replace the domain and FQDN in the reference.json file as this is tied to my setup, and won't work
+
+Token and Secret is required when you run the install.sh or setup.sh
+
+install.sh installs the dip-dup service which is a multi-domain service.
+setup.sh is a single sub-domain service.
---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+sudo apt install -y python3-pip
+
+read -p 'Token: ' domeneshop_token
+read -p 'Secret: ' domeneshop_secret
+
+sed -i "s/{TOKEN}/$domeneshop_token/g" service.py
+sed -i "s/{SECRET}/$domeneshop_secret/g" service.py
+
+
+pip install dnspython 
+pip install termcolor
+pip install requests
+pip install domeneshop
+
+systemctl stop dipdup.service
+systemctl disable dipdup.service
+
+rm /etc/systemd/system/dipdup.service
+
+systemctl daemon-reload
+
+sleep 10s
+
+mkdir --parents /usr/local/dipdup/
+cp ./service.py /usr/local/dipdup/service.py
+cp ./reference.json /usr/local/dipdup/reference.json
+
+cat > /etc/systemd/system/dipdup.service <<EOL
+[Unit]
+Description=Dynamic IP Service - Dns Updater
+
+[Service]
+Type=simple
+Restart=always
+ExecStart=/usr/bin/python3 -u /usr/local/dipdup/service.py
+Environment=PYTHONUNBUFFERED=1
+
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+
+chmod 700 /usr/local/dipdup/service.py
+chmod +x /usr/local/dipdup/service.py
+chown root:root /usr/local/dipdup/service.py
+
+
+systemctl daemon-reload
+
+systemctl enable dipdup.service
+systemctl start dipdup.service
+
+systemctl status dipdup.service
+
